f8c84be389d70486ab0fc04b088921bf5105bd0e
Resmiya <asirreshim0103@gmail.com>
2024-04-22 14:19:25 +0530

    file update

---
+++ sever/index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const mongoose = require("mongoose");
+
+const app = express();
+app.use(express.json());
+const port = 3001;
+
+app.use(express.json());
+const URI = "mongodb+srv://asirreshmi0103:asir0103@cluster0.ivhu6sq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+mongoose.connect(URI)
+  .then(() => {
+    console.log("Connected to the database!");
+  })
+  .catch((error) => {
+    console.log("Connection failed!", error);
+  });
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
